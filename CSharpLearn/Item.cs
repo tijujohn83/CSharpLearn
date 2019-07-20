89f1f13c1b9ab487e8899f0592066906a791e241
Tiju John <tijujohn83@gmail.com>
2019-07-20 20:46:12 +0200

    seed the new solution

---
+++ CSharpLearn/Item.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{
+    class Item
+    {
+        public ColorShade color { get; set; }
+    }
+
+    public enum ColorShade
+    {
+        White,
+        Black,
+        Red,
+        Blue,
+        Green
+    }
+
+    class ParentItem
+    {
+        public Item Item { get; set; }
+        public ParentItem(Item item)
+        {
+            Item = item;
+        }
+    }
+}
---
+++ CSharpLearn/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = GetItems();            
+            var blueItems2 = items.Where(IsBlue);
+            var parentItems1 = items.Select((x) => new ParentItem(x));
+            var parentItems2 = items.Select(GetParent);
+
+            items.ForEach(DoAction);
+
+            var isTrue = true;
+
+            isTrue = UseFunc(x => x > 0);
+            isTrue = UseExpression(x => x > 0);
+
+
+            isTrue = UseFunc(x => {
+                return x > 0;
+            });
+
+            isTrue = UseExpression(x => {
+                return x > 0;
+            });
+
+        }
+
+        public static void DoAction(Item item)
+        {
+          
+        }
+
+        public static bool UseFunc(Func<int, bool> method)
+        {
+            int a = 0;
+            return method(a);
+        }
+        public static bool UseExpression(Expression<Func<int, bool>> method)
+        {
+            int a = 0;
+            return method.Compile()(a);
+        }
+
+        private static bool IsBlue(Item item)
+        {
+            return item.color == ColorShade.Blue;
+        }
+
+        private static ParentItem GetParent(Item item)
+        {
+            return new ParentItem(item);
+        }
+
+        private static List<Item> GetItems()
+        {
+            var listItems = new List<Item>
+            {
+                new Item {color = ColorShade.Black },
+                new Item {color = ColorShade.Black },
+                new Item {color = ColorShade.Blue },
+                new Item {color = ColorShade.White },
+                new Item {color = ColorShade.Red },
+                new Item {color = ColorShade.Red },
+                new Item {color = ColorShade.White },
+                new Item {color = ColorShade.White },
+                new Item {color = ColorShade.Red },
+                new Item {color = ColorShade.Red },
+                new Item {color = ColorShade.Green },
+                new Item {color = ColorShade.Green },
+                new Item {color = ColorShade.Green },
+            };
+            return listItems;
+        }
+    }
+}
